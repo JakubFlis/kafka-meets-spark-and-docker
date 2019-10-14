7bc146c83e427725b01136535f41a26830f645f1
Developer <softwaredeveloper@developersoftware.com>
2019-10-14 21:59:14 +0200

    Simplify Hive init script

--- docker-environment/images/hive/init/hive_init.sh
+++ docker-environment/images/hive/init/hive_init.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-echo "Hive data set up started..."
 echo "Create and populate Hive tables"
 
 hive -f /root/hive/init/create_tables.sql
 
-for input_data_dir in $(find /root/hive/init/data_init_files/rating_portal -mindepth 1 -maxdepth 1 -type d); do
-    targetDir="$(basename $input_data_dir)"
-    mkdir -p /data/hive/warehouse/rating_portal.db/${targetDir,,}
-    cp -r $input_data_dir/* /data/hive/warehouse/rating_portal.db/${targetDir,,}/
-done
+mkdir -p /data/hive/warehouse/rating_portal.db/book_ratings
+cp -r $input_data_dir/* /data/hive/warehouse/rating_portal.db/book_ratings/
 
 echo "Hive data set up finished."
